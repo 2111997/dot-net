019580f2fb9a7eddc01d1900b55189f4323fd764
Your Name <snehaghongade123@gmail.com>
2022-06-03 11:18:14 +0530

    origin main

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+
+    public class Car : IVehicle
+    {
+        //property
+        public int Fuel 
+        { 
+            get;
+            set;
+        }
+        //constructor
+
+        public Car(int fuel)
+        {
+            Fuel = fuel;
+        }
+
+        //method
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+            else
+            {
+                Console.WriteLine("Not fuel");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            Fuel =Fuel + amount;
+            return true;
+        }
+    }
+
+
+
+
+
+    class Class1
+        {
+        public static void Main()
+        {
+                Car car = new Car(0);
+
+                int fuel = Convert.ToInt32(Console.ReadLine());
+                if (car.Refuel(fuel))
+                {
+                    car.Drive();
+                }
+
+
+
+            }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+
+
+    //interface
+    interface vehicle
+
+    {
+        void changegear(int a);
+        void speedup(int a);
+        void applybreaks(int a);
+    }
+
+    class Bicycle : vehicle
+    {
+        int speed;
+        int gear;
+
+        public void applybreaks(int decrement)
+        {
+            speed = speed - decrement;
+        }
+        public void changegear(int newgear)
+        {
+            gear = newgear;
+
+        }
+        public void speedup(int increment)
+        {
+            speed = speed + increment;
+        }
+
+        public void PrintStates()
+        {
+            Console.WriteLine("Bicycle Speed " + speed + " Bicycle Gear: " + gear);
+        }
+
+     }
+
+    class Bike : vehicle
+    {
+        int speed;
+        int gear;
+
+        public void applybreaks(int decrement)
+        {
+            speed = speed - decrement;
+        }
+        public void changegear(int newgear)
+        {
+            gear = newgear;
+
+        }
+        public void speedup(int increment)
+        {
+            speed = speed + increment;
+        }
+
+        public void PrintStates()
+        {
+            Console.WriteLine("Bike Speed " + speed + " Bike Gear: " + gear);
+        }
+
+    }
+
+ class Program
+    {
+        static void Main1(string[] args)
+        {
+            Bicycle b = new Bicycle();
+            b.changegear(2);
+            b.speedup(3);
+            b.applybreaks(1);
+            b.PrintStates();
+
+            Bike b1 = new Bike();
+            b1.changegear(2);
+            b1.speedup(3);
+            b1.applybreaks(1);
+            b1.PrintStates();
+
+
+
+
+
+
+
+
+
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
